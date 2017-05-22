9bb34792d3a2b3380f7a42e07e26c0c0c5f50b43
ahmed elbawab <ahmed.elbawab515@gmail.com>
2017-05-22 17:03:16 +0200

    MAZE MODIFY FX

--- src/eg/edu/alexu/csd/datastructure/maze/cs08/Main.java
+++ src/eg/edu/alexu/csd/datastructure/maze/cs08/Main.java
@@ -1,17 +1,6 @@
 package eg.edu.alexu.csd.datastructure.maze.cs08;
 
-import java.io.File;
-
-import java.io.IOException;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-public class Main /* extends Application */ {
+public class Main  {
 
 	public static void main(String[] args) {
 
